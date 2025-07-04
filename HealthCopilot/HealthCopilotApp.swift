78abbb3409bcfcd28c85696a349c414f82b8938f
natlydrad <natradalie@gmail.com>
2025-07-03 20:36:48 -0400

    Initial Commit

---
+++ HealthCopilot/HealthCopilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCopilotApp.swift
+//  HealthCopilot
+//
+//  Created by Natalie Radu on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthCopilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
